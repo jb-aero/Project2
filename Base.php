97a2e13ea8f40cd4783d3021c0f5b1fa71f15656
BurningLights <ben2701@verizon.net>
2015-11-21 16:16:40 -0500

    Fixing bugs in base

--- Base.php
+++ Base.php
@@ -3,7 +3,6 @@ require_once 'CommonMethods.php';
 include_once 'COEProject/Conversion.php';
 class Base {
 	// All these are meant to be PRIVATE!! DON'T MESS WITH THEM!
-	var $debug = false;
 	var $COMMON = null; // Don't create COMMON until necessary
 	var $info = array();
 	var $recordExists = false;
@@ -13,27 +12,27 @@ class Base {
 	// in the column specified by $field, from the table specified by $table
 	// Can pass array to $id to create from already queried row
 	function Base($common, $id, $table, $field='id') {
-		$this->$COMMON = $common;
+		$this->COMMON = $common;
 		if (is_array($id)) {
 			// Given info array from some other query
-			$this->$info = $id;
-			$this->$recordExists = true;
+			$this->info = $id;
+			$this->recordExists = true;
 			return;
 		}
 
 		// Query database for record from table with id
-		$rs = $this->$COMMON->executeQuery("SELECT * FROM `$table` WHERE `$field`='$id'", $_SEVER["PHP_SELF"]);
+		$rs = $this->COMMON->executeQuery("SELECT * FROM `$table` WHERE `$field`='$id'", $_SEVER["PHP_SELF"]);
 		if ($rs) {
 			// Successful query - set info and cache
-			$this->$info = mysqli_fetch_assoc($rs);
-			$this->$recordExists = true;
+			$this->info = mysql_fetch_assoc($rs);
+			$this->recordExists = true;
 		}
 	}
 	
 	// Get a specific piece of information
 	function getInfo($key) {
-		if (isset($this->$info[$key])) {
-			return $this->$info[$key];
+		if (isset($this->info[$key])) {
+			return $this->info[$key];
 		} else {
 			// Requested key not in info
 			return false;
@@ -46,25 +45,16 @@ class Base {
 	
 	// Whether the given record exists in the database or not
 	function exists() {
-		return $this->$recordExists;
+		return $this->recordExists;
 	}
 	
 	// Do a query using COMMON
 	function doQuery($query, $COMMON=null) {
 		if (isset($this)) {
-			$COMMON = $this->$COMMON;
+			$COMMON = $this->COMMON;
 		}
 		// Execute query using common
 		return $COMMON->executeQuery($query, $_SERVER['PHP_SELF']);
-	}
-	
-	// Set debug status
-	function setDebug($debug) {
-		// Set debug status, and update common, if it's been initalized
-		$this->$debug = $debug;
-		if ($this->$COMMON !== null) {
-			$this->$COMMON->$debug = $debug;
-		}
 	}
 }
 ?>
