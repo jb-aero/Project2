d702432871d2f8a0cee4d7fbcc4ffb55db311ea1
BurningLights <ben2701@verizon.net>
2015-11-24 20:35:45 -0500

    And fixing another typo

--- COEProject/08StudSelectTime.php
+++ COEProject/08StudSelectTime.php
@@ -39,7 +39,7 @@ if ($localAdvisor != 0) {
 		<form action = "10StudConfirmSch.php" method = "post" name = "SelectTime">
 	    <?php
 			// Hidden form field for advisor ID
-			echo "<input type='hidden' name='advisor' value='$localAdvisor'>"
+			echo "<input type='hidden' name='advisor' value='$localAdvisor'>";
 
 // http://php.net/manual/en/function.time.php fpr SQL statements below
 // Comparing timestamps, could not remember. 
