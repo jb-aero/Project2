f4cc1ec6522165e573a45537bc3aa7793d82d003
BurningLights <ben2701@verizon.net>
2015-11-28 14:56:16 -0500

    Updated student update page

--- COEProject/StudProcessEdit.php
+++ COEProject/StudProcessEdit.php
@@ -2,11 +2,6 @@
 session_start();
 include("Conversion.php");
 
-$_SESSION["firstN"] = strtoupper($_POST["firstN"]);
-$_SESSION["lastN"] = strtoupper($_POST["lastN"]);
-$_SESSION["email"] = $_POST["email"];
-$_SESSION["major"] = NameToAbb($_POST["major"]);
-
 $firstn = strtoupper($_POST["firstN"]);
 $lastn = strtoupper($_POST["lastN"]);
 $studid = $_SESSION["studID"];
@@ -16,10 +11,8 @@ $major = NameToAbb($_POST["major"]);
 $debug = false;
 include('../CommonMethods.php');
 $COMMON = new Common($debug);
-if($_SESSION["studExist"] == true){
-	$sql = "update `Proj2Students` set `FirstName` = '$firstn', `LastName` = '$lastn', `Email` = '$email', `Major` = '$major' where `StudentID` = '$studid'";
-	$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
-}
+$sql = "update `Proj2Students` set `FirstName` = '$firstn', `LastName` = '$lastn', `Email` = '$email', `Major` = '$major' where `StudentID` = '$studid'";
+$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 
 header('Location: 02StudHome.php');
 ?>
