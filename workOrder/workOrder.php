3ec6a03a66c599304edd71996f5535ff243834a7
Luke Carrico <carrico3@umbc.edu>
2015-11-28 23:31:39 -0500

    Update workOrder.php

--- workOrder/workOrder.php
+++ workOrder/workOrder.php
@@ -50,9 +50,11 @@ function stage1($_GET)
 	<form action="workOrder.php" method='post'>
 	<b><font size = 2px>Description:</font></b> <br><br><textarea name='description' id='description' rows="6" cols="100"></textarea><br><br>
 	<b><font size = 2px>Priority:</b></font> <br>
-			<input type="radio" name="priority" value="1" checked>1 (High) 
-			<input type="radio" name="priority" value="2">2 
-			<input type="radio" name="priority" value="3">3<br><br>
+			<input type="radio" name="priority" value="1" checked>1 (Highest)<br>
+			<input type="radio" name="priority" value="2">2<br>
+			<input type="radio" name="priority" value="3">3<br>
+			<input type="radio" name="priority" value="4">4<br>
+			<input type="radio" name="priority" value="5">5 (Lowest)<br>
 	
 	<input type="hidden" name="url" value='<?php echo($_GET["url"]); ?>'>
 
