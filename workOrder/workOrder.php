86c6f41f1fbdded022512dcdf9397ce66a7e96a9
Luke Carrico <carrico3@umbc.edu>
2015-11-28 23:29:14 -0500

    Update workOrder.php

--- workOrder/workOrder.php
+++ workOrder/workOrder.php
@@ -48,8 +48,8 @@ function stage1($_GET)
     <div class="top">
 
 	<form action="workOrder.php" method='post'>
-	<b><font size = 7px>Description:</font></b> <br><br><textarea name='description' id='description' rows="6" cols="100"></textarea><br><br>
-	<b><font size = 7px>Priority:</b></font> <br>
+	<b><font size = 2px>Description:</font></b> <br><br><textarea name='description' id='description' rows="6" cols="100"></textarea><br><br>
+	<b><font size = 2px>Priority:</b></font> <br>
 			<input type="radio" name="priority" value="1" checked>1 (High) 
 			<input type="radio" name="priority" value="2">2 
 			<input type="radio" name="priority" value="3">3<br><br>
