7881a0417fffa60b62fef4b61d701d41ba9b58a5
Luke Carrico <carrico3@umbc.edu>
2015-12-07 19:17:52 -0500

    Update AdminCreateNew.php

--- COEProject/AdminCreateNew.php
+++ COEProject/AdminCreateNew.php
@@ -26,30 +26,29 @@
 		<?php
 		
 	$_SESSION["PassCon"] = false;
+	$first = $_POST["firstN"];
+	$last = $_POST["lastN"];
+	$user = strtoupper($_POST["UserN"]);
+	$pass = md5($_POST["PassW"]);
+	$office = $_POST["Office"];
+	$meeting = $_POST["Meeting"];
+	echo($first);
+	$debug = false;
+	$COMMON = new Common($debug);
+	$advisor = new Advisor($COMMON, $user);
 	if ($_POST["PassW"] != $_POST["ConfP"]) {
 		$_SESSION["PassCon"] = true;
 		header('Location: AdminCreateNewAdv.php');
 	}
 	elseif ($_POST["PassW"] == $_POST["ConfP"]) {
-
-		$first = $_POST["firstN"];
-		$last = $_POST["lastN"];
-		$user = strtoupper($_POST["UserN"]);
-		$pass = md5($_POST["PassW"]);
-		$office = $_POST["Office"];
-		$meeting = $_POST["Meeting"];
-		echo($first);
-		$debug = false;
-		$COMMON = new Common($debug);
-		$advisor = new Advisor($COMMON, $user);
 			
-      //if ($advisor->createAdvisor($COMMON, $first, $last, $user, $pass, $office, $meeting)) {
+      if ($advisor->createAdvisor($COMMON, $first, $last, $user, $pass, $office, $meeting)) {
 		echo("<h2>New Advisor has been created:</h2>");
         echo ("<h3>$first $last<h3>");
-      //}
-     // else {
-       // echo("<h3>Advisor $first $last already exists</h3>");
-   //   }
+      }
+     else {
+       echo("<h3>Advisor $first $last already exists</h3>");
+    }
     }
 		?>
 		<form method="link" action="AdminUI.php">
