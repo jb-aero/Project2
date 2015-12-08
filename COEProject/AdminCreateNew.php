20d5a9fcdb6f3bbe2b5381bb5cb9004cd292a3de
Luke Carrico <carrico3@umbc.edu>
2015-12-07 23:51:19 -0500

    Update AdminCreateNew.php

--- COEProject/AdminCreateNew.php
+++ COEProject/AdminCreateNew.php
@@ -7,41 +7,39 @@
   <head>
     <meta charset="UTF-8" />
     <title>Create New Admin</title>
-    <script type="text/javascript">
-    function saveValue(target){
-	var stepVal = document.getElementById(target).value;
-	alert("Value: " + stepVal);
-    }
-    </script>
    <link rel='stylesheet' type='text/css' href='../css/standard.css'/>
   </head>
   <body>
+  	<?php 
+  	$_SESSION["PassCon"]=false;
+  	if ($_POST["PassW"] != $_POST["ConfP"])
+  	{
+		$_SESSION["PassCon"] = true;
+		header('Location: AdminCreateNewAdv.php');
+	}
+	?>
     <div id="login">
       <div id="form">
         <div class="top">
-		<h2>New Advisor has been created:</h2>
-
 		<?php
-			$first = $_SESSION["AdvF"];
-			$last = $_SESSION["AdvL"];
-			$user = $_SESSION["AdvUN"];
-			$pass = $_SESSION["AdvPW"];
+			$first = $_POST["firstName"];
+			$last = $_POST["lastName"];
+			$office = $_POST["Office"];
+			$meeting = $_POST["Meeting"];
+			$user = $_Post["UserN"];
+			$pass = $_SESSION["PassW"];
 
 			include('../CommonMethods.php');
 			$debug = false;
 			$Common = new Common($debug);
 
-      $sql = "SELECT * FROM `Proj2Advisors` WHERE `Username` = '$user' AND `FirstName` = '$first' AND  `LastName` = '$last'";
-      $rs = $Common->executeQuery($sql, "Advising Appointments");
-      $row = mysql_fetch_row($rs);
-      if($row){
-        echo("<h3>Advisor $first $last already exists</h3>");
+	$userN = $_SESSION["User_ID"];
+	$adv = new Advisor($Common,$userN);
+      if ($adv->createAdvisor($Common,$first,$last,$user,$pass,$office,$meeting))
+        echo("<h3>Advisor $first $last created successfully</h3>");
       }
       else{
-  			$sql = "INSERT INTO `Proj2Advisors`(`FirstName`, `LastName`, `Username`, `Password`) 
-  			VALUES ('$first', '$last', '$user', '$pass')";
-        echo ("<h3>$first $last<h3>");
-        $rs = $Common->executeQuery($sql, "Advising Appointments");
+      	echo("<h3>Advisor $first $last already exists</h3>");
       }
 		?>
 		<form method="link" action="AdminUI.php">
