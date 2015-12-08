e97f0cd0003247ee5e64fb5cac9df268ba6d9636
Luke Carrico <carrico3@umbc.edu>
2015-12-07 19:11:19 -0500

    Update Advisor.php

--- Advisor.php
+++ Advisor.php
@@ -37,6 +37,9 @@ class Advisor extends Base {
 	function getOffice() {
 		return $this->getInfo('Office');
 	}
+	function getMeeting() {
+		return $this->getInfo('Meeting');
+	}
 	
 	
 	/* Conversion functions */
@@ -49,6 +52,7 @@ class Advisor extends Base {
 	// the given first name, last name, and user name. Returns true on successful insert.
 	function createAdvisor($common, $firstName, $lastName, $username, $password, $office, $meeting) {
 		// Check if advisor already exists
+		echo("CREATE: ",$firstName);
 		$rs = $this->doQuery("SELECT `id` FROM Proj2Advisors WHERE `FirstName`='$firstName' AND
 		`LastName`='$lastName' AND `Username`='$username'", $common);
 		if (mysql_num_rows($rs) > 0) {
